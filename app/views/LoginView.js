c0905ae7bc0f5324b44b820ae628792618d1e323
Mikael Carlsson <mickec79@gmail.com>
2016-02-29 09:00:04 +0100

    state added to disable login button onSubmit

--- app/views/LoginView.js
+++ app/views/LoginView.js
@@ -1,7 +1,10 @@
 import React, { PropTypes } from 'react';
+import purebem from 'purebem';
 
 import firebaseRef from 'app/firebaseRef';
 
+
+const block = purebem.of('login');
 
 const LoginView = React.createClass({
 
@@ -9,9 +12,17 @@ const LoginView = React.createClass({
         router: PropTypes.object
     },
 
+    getInitialState() {
+        return {
+            isSubmitting: false
+        };
+    },
+
     onAuth(error, authData) {
+        this.setState({ isSubmitting: !this.state.isSubmitting });
+
         if (error) {
-            console.log(error);
+            this.setState({ error: error.message });
             return;
         }
 
@@ -20,8 +31,10 @@ const LoginView = React.createClass({
 
     onSubmit(evt) {
         evt.preventDefault();
-        const email = this.username.value;
+        const email = this.email.value;
         const password = this.password.value;
+
+        this.setState({ isSubmitting: !this.state.isSubmitting });
 
         firebaseRef.authWithPassword(
             { email, password },
@@ -30,21 +43,45 @@ const LoginView = React.createClass({
         );
     },
 
+    onChange() {
+        this.setState({ error: '' });
+    },
+
+    renderError() {
+        if (!this.state.error) {
+            return null;
+        }
+
+        return (
+            <div className={ block('error') }>
+                <span>Error: { this.state.error }</span>
+            </div>
+        );
+    },
+
+    renderPasswordLogin() {
+        const buttonClass = purebem.many(block('button', ['submit']), 'button-primary');
+
+        return (
+            <form className={ block('form') } onSubmit={ this.onSubmit }>
+                <label className={ block('group') }>
+                    <span className={ block('label') }>Email</span>
+                    <input type="text" className={ block('input') } onChange={ this.onChange } ref={ (ref) => this.email = ref } />
+                </label>
+                <label className={ block('group') }>
+                    <span className={ block('label') }>Password</span>
+                    <input type="password" className={ block('input') } ref={ (ref) => this.password = ref } />
+                </label>
+                <button type="submit" className={ buttonClass } disabled={ this.state.isSubmitting }>Login</button>
+                { this.renderError() }
+            </form>
+        );
+    },
+
     render() {
         return (
-            <div>
-                <h1>Login</h1>
-                <form onSubmit={ this.onSubmit }>
-                    <label>
-                        <span>Username</span>
-                        <input type="text" ref={ (ref) => this.username = ref } />
-                    </label>
-                    <label>
-                        <span>Password</span>
-                        <input type="password" ref={ (ref) => this.password = ref } />
-                    </label>
-                    <button type="submit">Login</button>
-                </form>
+            <div className={ purebem.many(block(), 'container') }>
+                { this.renderPasswordLogin() }
             </div>
         );
     }
