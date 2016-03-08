242a7b15e810259fdde2f63e244c0a55d5cc5a09
Mikael Carlsson <mickec79@gmail.com>
2016-03-08 01:03:45 +0100

    create project view updated

--- app/views/CreateView.js
+++ app/views/CreateView.js
@@ -5,6 +5,7 @@ import moment from 'moment';
 import firebaseRef from 'app/firebaseRef';
 
 import ProjectMap from 'app/components/ProjectMap';
+import ToggleButton from 'app/components/ToggleButton';
 
 
 const block = purebem.of('create-view');
@@ -13,19 +14,40 @@ const CreateView = React.createClass({
 
     getInitialState() {
         return {
+            dateEnd: '',
+            dateStart: '',
             isSubmitting: false,
-            sites: []
+            options: [
+                { text: 'Public', value: true },
+                { text: 'Private', value: false }
+            ],
+            sites: [],
+            title: ''
         };
     },
 
-    onMapClick(marker) {
-        const sites = [ ...this.state.sites, marker ];
-        this.setState({ sites });
+    onDateChange(name) {
+        return evt => {
+            const input = evt.target.value;
+            const isValid = moment(input, 'YYYYMMDD', true).isValid();
+            if (isValid) {
+                this.setState({ [name]: input });
+            }
+        };
     },
 
     onFormReset() {
         this.form.reset();
         this.sitesReset();
+    },
+
+    onFormSubmit() {
+
+    },
+
+    onMapClick(marker) {
+        const sites = [ ...this.state.sites, marker ];
+        this.setState({ sites });
     },
 
     onSiteChange(index) {
@@ -37,16 +59,41 @@ const CreateView = React.createClass({
     },
 
     onSiteRemove(index) {
-        let { sites } = this.state;
-        sites[index].setMap(null);
-        sites = sites.filter((item, i) => i !== index);
-        this.setState({ sites });
+        return evt => {
+            evt.preventDefault();
+            const sites = this.state.sites.filter((item, i) => {
+                if (i === index) {
+                    item.setMap(null);
+                }
+                return i !== index;
+            });
+            this.setState({ sites });
+        };
+    },
+
+    onToggleClick(index) {
+        const options = this.state.options.map((item, i) => {
+            item.value = i === index ? true : false;
+            return item;
+        });
+        this.setState({ options });
     },
 
     sitesReset() {
-        let { sites } = this.state;
-        sites = sites.filter(item => item.setMap(null));
+        const sites = this.state.sites.filter(item => item.setMap(null));
         this.setState({ sites });
+    },
+
+    renderPrivacyText() {
+        const active = this.state.options.filter(item => item.value === true);
+
+        if (active[0].text === 'Private') {
+            return <p>Private projects are a one man show. No one can find your project and you are on your own.</p>
+        }
+
+        return (
+            <p>Public projects aim at collaboration. Users can find your project and may request to join.</p>
+        );
     },
 
     renderMap() {
@@ -60,9 +107,9 @@ const CreateView = React.createClass({
     renderSite(item, index) {
         return (
             <label className={ block('group') } key={ index }>
-                <span className={ block('label') }>Name of Site { index + 1 }</span>
-                <input type="text" className={ block('input', ['removable']) } value={ item.value } onChange={ this.onSiteChange(index) } />
-                <div className={ block('remove') } onClick={ () => this.onSiteRemove(index) }>X</div>
+                <span className={ block('label') }>Site name { index + 1 }</span>
+                <input type="text" className={ block('input', ['icon']) } value={ item.value } onChange={ this.onSiteChange(index) } />
+                <i className={ block('remove') } onClick={ this.onSiteRemove(index) } />
             </label>
         );
     },
@@ -72,23 +119,31 @@ const CreateView = React.createClass({
 
         return (
             <form className={ block('form') } onSubmit={ this.onSubmit } ref={ (form) => this.form = form }>
+                <div className={ block('info') }>
+                    <p>Use the map to locate and mark ringing sites. The GPS coordinates can be used to track individual birds.</p>
+                </div>
                 <label className={ block('group') }>
                     <span className={ block('label') }>Project title</span>
-                    <input type="text" className={ block('input') } />
+                    <input type="text" className={ block('input') } onChange={ (evt) => this.setState({ title: evt.target.value }) } />
                 </label>
                 <label className={ block('group') }>
                     <span className={ block('label') }>Start date - YYYYMMDD</span>
-                    <input type="text" className={ block('input') } />
+                    <input type="text" className={ block('input', ['icon']) } onChange={ this.onDateChange('dateStart') } />
                 </label>
                 <label className={ block('group') }>
                     <span className={ block('label') }>End date - YYYYMMDD</span>
-                    <input type="text" className={ block('input') } />
+                    <input type="text" className={ block('input', ['icon']) } onChange={ this.onDateChange('dateEnd') } />
                 </label>
                 {
                     [].map.call(this.state.sites, this.renderSite)
                 }
+                <ToggleButton
+                    onClick={ this.onToggleClick }
+                    options={ this.state.options }
+                    className={ block('toggle-button') } />
+                <div className={ block('info') }>{ this.renderPrivacyText() }</div>
                 <div className={ block('actions') }>
-                    <button type="submit" className={ buttonClass } disabled={ this.state.isSubmitting }>Create Project</button>
+                    <button type="submit" className={ buttonClass } disabled={ this.state.isSubmitting } onClick={ this.onFormSubmit }>Create</button>
                     <button type="button" className={ block('button', ['reset']) } onClick={ this.onFormReset }>Reset</button>
                 </div>
             </form>
@@ -96,6 +151,7 @@ const CreateView = React.createClass({
     },
 
     render() {
+        console.log(this.state);
         return (
             <div className={ block() }>
                 <div className="container">
