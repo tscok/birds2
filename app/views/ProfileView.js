a9066aee929becf32f0f9025541edcbb659d186c
Mikael Carlsson <mickec79@gmail.com>
2016-03-30 23:09:32 +0200

    loading the projects a user is in

--- app/views/ProfileView.js
+++ app/views/ProfileView.js
@@ -2,29 +2,73 @@ import React from 'react';
 import purebem from 'purebem';
 import assign from 'lodash.assign';
 
-import firebaseRef from 'app/firebaseRef';
+import promise from 'promise';
 
-import Projects from 'app/components/Projects';
+import firebaseRef from 'app/firebaseRef';
+import ReactFireMixin from 'reactfire';
+
+import ProjectList from 'app/components/ProjectList';
 
 
 const block = purebem.of('profile-view');
 
 const ProfileView = React.createClass({
 
+    mixins: [ReactFireMixin],
+
     getInitialState() {
         return {
+            projects: [],
+            isLoading: true
         };
     },
 
     componentWillMount() {
-        this.authData = firebaseRef.getAuth();
-        this.userData = this.authData[this.authData.provider];
+        const uid = firebaseRef.getAuth().uid;
+
+        this.userProjectsRef = firebaseRef.child(`users/${uid}/projects`);
+        this.projectsRef = firebaseRef.child('projects');
+
+        this.userProjectsRef.on('value', (snap) => {
+            if (snap.numChildren() > 0) {
+                this.getProjects(snap);
+            } else {
+                this.setState({ isLoading: false });
+            }
+        });
+    },
+
+    componentWillUnmount() {
+        this.userProjectsRef.off('value');
+    },
+
+    getProjects(snap) {
+        const projects = [];
+        snap.forEach((item) => {
+            this.getProject(item.key()).then(result => {
+                projects.push(result);
+
+                if (projects.length === snap.numChildren()) {
+                    this.setState({ projects, isLoading: false });
+                }
+            });
+        });
+    },
+
+    getProject(id) {
+        return new promise((resolve, reject) => {
+            this.projectsRef.child(id).once('value', (child) => {
+                const data = child.val();
+                data.projectId = child.key();
+                resolve(data);
+            });
+        });
     },
 
     render() {
         return (
             <div className={ block() }>
-                <Projects />
+                <ProjectList projects={ this.state.projects } isLoading={ this.state.isLoading } />
             </div>
         );
     }
