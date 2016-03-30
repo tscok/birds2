d9bf01cd29c00a9aea92d78d9b5de01ae685311c
Mikael Carlsson <mickec79@gmail.com>
2016-03-30 22:51:46 +0200

    broke off site listing into its own component, may come in handy for future "edit project"

---
+++ app/components/ProjectSites.js
@@ -0,0 +1,38 @@
+import React, { PropTypes } from 'react';
+
+import InputField from './InputField';
+
+
+const ProjectSites = React.createClass({
+
+    propTypes: {
+        sites: PropTypes.array.isRequired,
+        onChange: PropTypes.func.isRequired,
+        onRemove: PropTypes.func.isRequired
+    },
+
+    renderSite(item, index) {
+        const siteNr = index + 1;
+        return (
+            <div className="form__group" key={ index }>
+                <label>Site name { siteNr }</label>
+                <InputField
+                    iconClass="icon-cross"
+                    iconClick={ () => this.props.onRemove(index) }
+                    onChange={ this.props.onChange(index) }
+                    value={ item.name } />
+            </div>
+        );
+    },
+
+    render() {
+        return (
+            <div>
+                { [].map.call(this.props.sites, this.renderSite) }
+            </div>
+        );
+    }
+
+});
+
+export default ProjectSites;
