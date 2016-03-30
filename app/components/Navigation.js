0a16bf5b9bb7c054aaeae038bd160f49788d29a0
Mikael Carlsson <mickec79@gmail.com>
2016-03-31 00:18:30 +0200

    add "home" links to navigation

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -51,6 +51,7 @@ const Navigation = React.createClass({
 
         return (
             <nav className={ block('links', ['profile']) }>
+                <NavLink base={ block('link') } to="/profile">Profile</NavLink>
                 <NavLink base={ block('link') } to="/create">Create</NavLink>
                 <NavLink base={ block('link') } to="/search">Search</NavLink>
             </nav>
@@ -66,10 +67,13 @@ const Navigation = React.createClass({
 
         return (
             <nav className={ block('links', ['project']) }>
+                <NavLink base={ block('link') } to={ `/project/${id}` }>Dashboard</NavLink>
                 <NavLink base={ block('link') } to={ `/project/${id}/entry` }>New Entry</NavLink>
+            {/*
                 <NavLink base={ block('link') } to={ `/project/${id}/members` }>Members</NavLink>
                 <NavLink base={ block('link') } to={ `/project/${id}/rings` }>Rings</NavLink>
                 <NavLink base={ block('link') } to={ `/project/${id}/export` }>Export Data</NavLink>
+            */}
             </nav>
         );
     },
