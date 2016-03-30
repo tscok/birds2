35a2f5bcef2069609a84f4fc8838e96f66c5bb68
Mikael Carlsson <mickec79@gmail.com>
2016-03-31 00:45:53 +0200

    loading basic project data

--- app/views/ProjectView.js
+++ app/views/ProjectView.js
@@ -1,15 +1,73 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import purebem from 'purebem';
 
+import firebaseRef from 'app/firebaseRef';
+
+import ContentBox from 'app/components/ContentBox';
+import Spinner from 'app/components/Spinner';
+
+
+const block = purebem.of('project-view');
 
 const ProjectView = React.createClass({
+
+    propTypes: {
+        params: PropTypes.object
+    },
+
+    contextTypes: {
+        router: PropTypes.object
+    },
+
+    getInitialState() {
+        return {
+            isLoading: true
+        };
+    },
+
+    componentWillMount() {
+        const uid = firebaseRef.getAuth().uid;
+        const pid = this.props.params.id;
+
+        this.validateMembership(pid, uid);
+
+        firebaseRef.child(`projects/${this.props.params.id}`).once('value', (snap) => {
+            this.setState({ project: snap.val(), isLoading: false });
+        });
+    },
+
+    validateMembership(pid, uid) {
+        firebaseRef.child(`memberships/${pid}/member/${uid}`).once('value', (snap) => {
+            if (snap.val() !== true) {
+                this.context.router.push('/profile');
+            }
+        });
+    },
+
+    renderProject() {
+        if (this.state.isLoading) {
+            return <Spinner />
+        }
+
+        console.log(this.state.project);
+
+        return (
+            <div className="container">
+                <ContentBox title={ this.state.project.title } background="white" shadow={ true }>
+                    <p>Test</p>
+                </ContentBox>
+            </div>
+        );
+    },
+
     render() {
         return (
-            <div>
-                <h1>Project</h1>
-                { this.props.children }
+            <div className={ block() }>
+                { this.renderProject() }
             </div>
         );
     }
+
 });
 
 export default ProjectView;
