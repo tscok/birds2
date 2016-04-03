ada7d11da5b54a71b2d4266f2e8d7a2060315470
Mikael Carlsson <mickec79@gmail.com>
2016-04-04 00:16:03 +0200

    success state cleanup

--- app/components/ProjectSuccess.js
+++ app/components/ProjectSuccess.js
@@ -27,16 +27,18 @@ const ProjectSuccess = React.createClass({
 
     render() {
         const primary = purebem.many(block('button', ['primary']), 'button', 'button-primary');
+        
         return (
             <div className={ block() }>
                 <div className={ block('header') }>
                     <i className={ block('icon') } />
-                    {/*<div className={ block('title') }>Good Job!</div>*/}
                 </div>
                 <div className={ block('body') }>
-                    <p className={ block('body-copy') }><strong>The project was successfully created!</strong><br />How would you like to proceed?</p>
-                    <button className={ primary } onClick={ this.handleReroute }>Go to project</button>
-                    <button className={ block('button') } onClick={ this.props.onClose }>Create another</button>
+                    <p><strong>The project was successfully created!</strong><br />How would you like to proceed?</p>
+                    <div className={ block('actions') }>
+                        <button className={ primary } onClick={ this.handleReroute }>Go to project</button>
+                        <button className={ block('button') } onClick={ this.props.onClose }>Create another</button>
+                    </div>
                 </div>
             </div>
         );
