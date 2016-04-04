2758a04829df3e0c49b26d9ef80d03d660c6b9e8
Mikael Carlsson <mickec79@gmail.com>
2016-04-04 08:59:59 +0200

    setting up search

--- app/utils.js
+++ app/utils.js
@@ -1,6 +1,15 @@
 import moment from 'moment';
+import debounce from 'lodash.debounce';
 
-export function getStatus(start, end) {
+
+const delayAction = debounce((action) => action(), 300);
+
+function getStatus(start, end) {
     const now = moment().unix();
     return now > start ? ( now < end ? 'Active' : 'Ended' ) : 'Pending';
+};
+
+export {
+    getStatus,
+    delayAction
 };
--- app/views/SearchView.js
+++ app/views/SearchView.js
@@ -1,12 +1,77 @@
 import React from 'react';
+import purebem from 'purebem';
 
+import { delayAction } from 'app/utils';
+
+import firebaseRef from 'app/firebaseRef';
+
+import ContentBox from 'app/components/ContentBox';
+import InputField from 'app/components/InputField';
+
+
+const block = purebem.of('search-view');
 
 const SearchView = React.createClass({
+
+    getInitialState() {
+        return {
+            needle: '',
+            results: []
+        };
+    },
+
+    componentWillMount() {
+        this.projectsRef = firebaseRef.child('projects').orderByChild('public').equalTo(true);
+        this.sitesRef = firebaseRef.child('sites');
+    },
+
+    handleSearch() {
+        // this.projectsRef.child('title')
+    },
+
+    handleChange(evt) {
+        const needle = evt.target.value;
+        this.setState({ needle });
+
+        delayAction(() => {
+            this.handleSearch()
+        });
+    },
+
+    renderResults() {
+        if (!this.state.results.length) {
+            return null;
+        }
+
+        return (
+            <div className="container">
+                <ContentBox title="Results">
+                    [results]
+                </ContentBox>
+            </div>
+        );
+    },
+
     render() {
         return (
-            <h1>Search</h1>
+            <div className={ block() }>
+                <div className={ block('intro') }>
+                    <div className={ block('title') }>Find &amp; Join</div>
+                    <div className={ block('body') }>
+                        <p>Here you can search for existing projects to join.</p>
+                        <InputField
+                            autoFocus={ true }
+                            iconClass="icon-search"
+                            iconClick={ this.handleSearch }
+                            onChange={ this.handleChange }
+                            value={ this.state.needle } />
+                    </div>
+                </div>
+                { this.renderResults() }
+            </div>
         );
     }
+
 });
 
 export default SearchView;
