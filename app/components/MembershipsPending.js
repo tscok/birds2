13c553250b8191756411d8d944176791d94d261b
Mikael Carlsson <mickec79@gmail.com>
2016-04-16 17:37:11 +0200

    memberships pending counter

---
+++ app/components/MembershipsPending.js
@@ -0,0 +1,29 @@
+import React, { PropTypes } from 'react';
+import purebem from 'purebem';
+
+
+const block = purebem.of('memberships-pending');
+
+const MembershipsPending = React.createClass({
+
+    propTypes: {
+        ownerId: PropTypes.string.isRequired,
+        pending: PropTypes.number.isRequired,
+        userId: PropTypes.string.isRequired
+    },
+
+    render() {
+        const { ownerId, pendingCount, userId } = this.props;
+
+        if (ownerId !== userId || pending === 0) {
+            return null;
+        }
+
+        return (
+            <span className={ block() }>{ pending }</span>
+        );
+    }
+
+});
+
+export default MembershipsPending;
