04107c3168f80b52a3a9d21e8277938fc4490c9e
Mikael Carlsson <mickec79@gmail.com>
2016-04-16 17:43:45 +0200

    general updates

--- app/utils.js
+++ app/utils.js
@@ -6,7 +6,7 @@ const delayAction = debounce(action => action(), 300);
 
 function getStatus(start, end) {
     const now = moment().unix();
-    return now > start ? ( now < end ? 'Active' : 'Ended' ) : 'Pending';
+    return now > start ? ( now < end ? 'Active' : 'Ended' ) : 'Coming';
 };
 
 function isEmpty(str) {
