cd8389e8a8090a7e0dafda04836bc8a11479c3b3
Mikael Carlsson <mickec79@gmail.com>
2016-04-16 17:40:10 +0200

    updated Avatar to include URL

--- app/components/Avatar.js
+++ app/components/Avatar.js
@@ -8,22 +8,50 @@ const Avatar = React.createClass({
 
     propTypes: {
         name: PropTypes.string,
-        status: PropTypes.string
+        status: PropTypes.string,
+        url: PropTypes.string
     },
 
     getDefaultProps() {
         return {
-            name: 'Avatar',
-            status: ''
+            name: null,
+            status: null,
+            url: null
         };
     },
 
-    render() {
-        const { name, status } = this.props;
-        const letter = name.substring(0,1);
+    renderLetter() {
+        if (this.props.url || !this.props.name) {
+            return null;
+        }
+
+        const letter = this.props.name.substring(0,1);
 
         return (
-            <div className={ block({ status: status.toLowerCase() }) }>{ letter }</div>
+            <div className={ block('letter') }>{ letter }</div>
+        );
+    },
+
+    renderImage() {
+        if (!this.props.url) {
+            return null;
+        }
+
+        return (
+            <img className={ block('portrait') } src={ this.props.url } />
+        );
+    },
+
+    render() {
+        let { status } = this.props;
+
+        return (
+            <div className={ block({ status: !!status }) }>
+                <div className={ block('badge', { status: !!status && status.toLowerCase() }) }>
+                    { this.renderImage() }
+                </div>
+                { this.renderLetter() }
+            </div>
         );
     }
 
