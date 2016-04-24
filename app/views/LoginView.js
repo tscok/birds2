fe68abfa0e4880eacc05eed3e276ecc24cc19d17
Mikael Carlsson <mickec79@gmail.com>
2016-04-24 23:10:47 +0200

    update login logic to facilitate facebook login

--- app/views/LoginView.js
+++ app/views/LoginView.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import purebem from 'purebem';
+import promise from 'promise';
 
 import firebaseRef from 'app/firebaseRef';
 
@@ -24,31 +25,51 @@ const LoginView = React.createClass({
         };
     },
 
-    onAuth(error, authData) {
-        this.setState({ isSubmitting: !this.state.isSubmitting });
-
-        if (error) {
-            this.setState({ error: error.message });
-            return;
-        }
-
-        this.context.router.push('/profile');
+    passwordLogin(userData) {
+        return new promise((resolve, reject) => {
+            firebaseRef.authWithPassword(userData, (error, authData) => {
+                if (error) reject(error);
+                else resolve(authData);
+            });
+        });
     },
 
-    onSubmit(evt) {
-        evt.preventDefault();
-        const { email, password } = this.state;
+    socialLogin(provider) {
+        return new promise((resolve, reject) => {
+            firebaseRef.authWithOAuthPopup(provider, (error, authData) => {
+                if (error) reject(error);
+                else resolve(authData);
+            })
+        });
+    },
 
-        this.setState({ isSubmitting: !this.state.isSubmitting, error: '' });
-
-        firebaseRef.authWithPassword(
-            { email, password },
-            this.onAuth,
-            { remember: 'sessionOnly' }
+    handleAuthResponse(loginPromise) {
+        this.setState({ isSubmitting: false });
+        loginPromise.then(
+            (authData) => {
+                this.context.router.push('/profile');
+            },
+            (error) => {
+                this.setState({ error: error.message });
+            }
         );
     },
+    
+    handleSocialClick(provider) {
+        const loginPromise = this.socialLogin(provider);
+        this.handleAuthResponse(loginPromise);
+    },
 
-    onChange(evt) {
+    handleSubmit(evt) {
+        evt.preventDefault();
+        const { email, password, isSubmitting } = this.state;
+        const loginPromise = this.passwordLogin({ email, password });
+
+        this.setState({ isSubmitting: !isSubmitting, error: '' });
+        this.handleAuthResponse(loginPromise);
+    },
+
+    handleChange(evt) {
         const { name, value } = evt.target;
         this.setState({ [name]: value, error: '' });
     },
@@ -69,28 +90,26 @@ const LoginView = React.createClass({
         if (!this.state.isSubmitting) {
             return null;
         }
-
         return <Spinner />;
     },
 
     renderPasswordLogin() {
         const buttonClass = purebem.many(block('button', ['submit']), 'button-primary');
-
         return (
             <div className={ block('password-login') }>
-                <form onSubmit={ this.onSubmit }>
+                <form onSubmit={ this.handleSubmit }>
                     <div className="form__group">
                         <label>Email</label>
                         <InputField
                             name="email"
-                            onChange={ this.onChange }
+                            onChange={ this.handleChange }
                             value={ this.state.email } />
                     </div>
                     <div className="form__group">
                         <label>Password</label>
                         <InputField
                             name="password"
-                            onChange={ this.onChange }
+                            onChange={ this.handleChange }
                             type="password"
                             value={ this.state.password } />
                     </div>
@@ -102,11 +121,21 @@ const LoginView = React.createClass({
         );
     },
 
+    renderFacebookLogin() {
+        const buttonClass = purebem.many(block('button', ['facebook']), 'button-primary');
+        return (
+            <div className={ block('social-login') }>
+                <button type="button" className={ buttonClass } onClick={ () => this.handleSocialClick('facebook') }>Login with Facebook</button>
+            </div>
+        );
+    },
+
     render() {
         return (
             <div className={ block() }>
                 <ContentBox>
                     { this.renderPasswordLogin() }
+                    { this.renderFacebookLogin() }
                 </ContentBox>
             </div>
         );
