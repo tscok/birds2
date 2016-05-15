cb4096b99829bb8dea1c99a85c7d43def9d70886
Mikael Carlsson <mickec79@gmail.com>
2016-05-16 00:07:58 +0200

    project view update in progress…

--- app/views/ProjectView.js
+++ app/views/ProjectView.js
@@ -38,59 +38,60 @@ const ProjectView = React.createClass({
         const uid = firebaseRef.getAuth().uid;
         const pid = this.props.params.id;
 
-        this.membersRef = firebaseRef.child(`members/${pid}/active/${uid}`);
+        // this.membersRef = firebaseRef.child(`members/${pid}/active/${uid}`);
         this.projectRef = firebaseRef.child(`projects/${this.props.params.id}`);
 
-        this.memberAuth().then(() => {
+        // this.memberAuth().then(() => {
             this.projectRef.once('value', (snap) => {
+                console.log(snap.val());
                 this.setState({ project: snap.val(), isLoading: false });
             });
-        });
+        // });
     },
 
-    memberAuth() {
-        return new promise((resolve, reject) => {
-            this.membersRef.once('value', (snap) => {
-                if (!snap.exists()) {
-                    this.context.router.push('/profile');
-                    return;
-                }
-                resolve();
-            });
-        });
-    },
+    // memberAuth() {
+    //     return new promise((resolve, reject) => {
+    //         this.membersRef.once('value', (snap) => {
+    //             if (!snap.exists()) {
+    //                 this.context.router.push('/profile');
+    //                 return;
+    //             }
+    //             resolve();
+    //         });
+    //     });
+    // },
 
-    renderStatus() {
-        const { title, dateStart, dateEnd } = this.state.project;
-        const status = getStatus(dateStart, dateEnd).toLowerCase();
+    // renderStatus() {
+    //     const { title, dateStart, dateEnd } = this.state.project;
+    //     const status = getStatus(dateStart, dateEnd).toLowerCase();
 
-        let body;
+    //     let body;
 
-        switch (status) {
-            case 'active':
-                body = (<p>This project will end { moment.unix(dateEnd).format('MMMM D, YYYY') }.</p>);
-                break;
-            case 'pending':
-                body = (<p>This project will start { moment.unix(dateStart).format('MMMM D, YYYY') }.</p>);
-                break;
-            default:
-                body = (<p>This project ended { moment.unix(dateEnd).format('MMMM D, YYYY') }.</p>);
-        };
+    //     switch (status) {
+    //         case 'active':
+    //             body = (<p>This project will end { moment.unix(dateEnd).format('MMMM D, YYYY') }.</p>);
+    //             break;
+    //         case 'pending':
+    //             body = (<p>This project will start { moment.unix(dateStart).format('MMMM D, YYYY') }.</p>);
+    //             break;
+    //         default:
+    //             body = (<p>This project ended { moment.unix(dateEnd).format('MMMM D, YYYY') }.</p>);
+    //     };
 
-        return (<ViewHeader title={ title }>{ body }</ViewHeader>);
-    },
+    //     return (<ViewHeader title={ title }>{ body }</ViewHeader>);
+    // },
 
-    renderCharts() {
-        return (
-            <div className={ block('charts')}>
-                <div className="container">
-                    <div className="one-third column">[chart]</div>
-                    <div className="one-third column">[chart]</div>
-                    <div className="one-third column">[chart]</div>
-                </div>
-            </div>
-        );
-    },
+    // renderCharts() {
+    //     return (
+    //         <div className={ block('charts')}>
+    //             <div className="container">
+    //                 <div className="one-third column">[chart]</div>
+    //                 <div className="one-third column">[chart]</div>
+    //                 <div className="one-third column">[chart]</div>
+    //             </div>
+    //         </div>
+    //     );
+    // },
 
     render() {
         if (this.state.isLoading) {
@@ -99,8 +100,7 @@ const ProjectView = React.createClass({
 
         return (
             <div className={ block() }>
-                { this.renderStatus() }
-                { this.renderCharts() }
+                { this.state.project.title }
             </div>
         );
     }
