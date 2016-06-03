e824e37e556545e1db3db02926e055448eee3378
Mikael Carlsson <mickec79@gmail.com>
2016-06-03 23:35:20 +0200

    add state to App to handle re-auth

--- app/App.js
+++ app/App.js
@@ -1,15 +1,59 @@
 import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
 import purebem from 'purebem';
 
+import { firebase, getUser } from 'app/firebase';
+
 import { LoginView, Navigation } from './views';
+import { ModalContainer } from './components';
+
+import { userUpdate } from 'app/redux/actions';
 
 
 const block = purebem.of('app-view');
 
 const App = React.createClass({
 
+    contextTypes: {
+        router: PropTypes.object
+    },
+
     propTypes: {
-        children: PropTypes.node
+        onRefresh: PropTypes.func.isRequired,
+        // ...
+        children: PropTypes.node,
+        location: PropTypes.object
+    },
+
+    componentWillMount() {
+        // Refresh user state on reload if logged in.
+        firebase.auth().onAuthStateChanged((authData) => {
+            if (authData) {
+                this.props.onRefresh(getUser(authData));
+            } else {
+                console.log('no user found');
+            }
+        });
+    },
+
+    componentWillUpdate() {
+        const { pathname } = this.props.location;
+
+        // Prevent routing away from login if logged out.
+        if (pathname === '/login' && !firebase.auth().currentUser) {
+            this.context.router.push('/login');
+        }
+    },
+
+    renderContent() {
+        if (this.props.location.pathname === '/login') {
+            return (
+                <ModalContainer>
+                    <LoginView />
+                </ModalContainer>
+            );
+        }
+        return this.props.children;
     },
 
     render() {
@@ -17,7 +61,7 @@ const App = React.createClass({
             <div className={ block() }>
                 <Navigation { ...this.props } />
                 <main className={ block('main') }>
-                    { this.props.children }
+                    { this.renderContent() }
                 </main>
             </div>
         );
@@ -25,4 +69,10 @@ const App = React.createClass({
 
 });
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onRefresh: (user) => dispatch(userUpdate(user))
+    };
+};
+
+export default connect(null, mapDispatchToProps)(App);
