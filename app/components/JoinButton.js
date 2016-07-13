57e3362d8ff7d4a3bfc83bbcbd9296bbe5925165
Mikael Carlsson <mickec79@gmail.com>
2016-07-14 00:44:49 +0200

    join button WIP

--- app/components/JoinButton.js
+++ app/components/JoinButton.js
@@ -5,95 +5,78 @@ import purebem from 'purebem';
 import { firebase } from 'app/firebase';
 import { Spinner } from 'app/components';
 
-import { joinUpdate } from 'app/redux/join';
-import { searchUpdate, resultUpdate } from 'app/redux/search';
-
 
 const block = purebem.of('join-button');
 
 const JoinButton = React.createClass({
 
-    // propTypes: {
-    //     index: PropTypes.number.isRequired,
-    //     loading: PropTypes.bool.isRequired,
-    //     onUpdate: PropTypes.func.isRequired,
-    //     project: PropTypes.object.isRequired,
-    //     user: PropTypes.shape({
-    //         uid: PropTypes.string,
-    //         name: PropTypes.string,
-    //         email: PropTypes.string,
-    //         photoURL: PropTypes.string
-    //     }).isRequired,
-    //     // ...
-    //     role: PropTypes.string
-    // },
+    propTypes: {
+        project: PropTypes.object.isRequired,
+        user: PropTypes.object.isRequired
+    },
+
+    getInitialState() {
+        return {
+            loading: true
+        };
+    },
 
     componentWillMount() {
-        // console.log('button mounting');
-        // const { project, user } = this.props;
-
-        // console.log('button will mount', project);
-
-        // this.memberRef = firebase.database().ref(`members/${project.pid}/${user.uid}/role`);
-
-        // this.memberRef.on('value', (snap) => {
-        //     console.log('role', snap.val());
-        //     this.props.onUpdate(snap.val());
-        // });
-    },
-
-    componentWillUnmount() {
-        // this.memberRef.off('value');
-    },
-
-    componentDidMount() {
-        // console.log(this.props);
-    },
-
-    isProjectOwner() {
-        return this.props.user.uid === this.props.project.uid;
-    },
-
-    handleClick() {
         const { project, user } = this.props;
-        const userData = {
-            ...user,
-            role: 'pending'
-        };
-        const projectData = {
-            ...project,
-            role: 'pending'
-        }
-
-        firebase.database().ref(`members/${project.pid}/${user.uid}`).set(userData);
-        firebase.database().ref(`users/${user.uid}/projects/${project.pid}`).set(projectData);
+        this.projectRef = firebase.database().ref(`users/${user.uid}/projects/${project.id}`);
+        this.projectRef.on('value', this.handleSnap);
     },
 
-    renderStatus() {
-        if (!this.props.project.role) {
-            return null;
+    handleSnap(snap) {
+        if (!snap.exists()) {
+            this.setState({ loading: false });
+            return;
         }
-        return (
-            <span className={ block('status') }>{ this.props.project.role }</span>
-        );
+        
+        const { role } = snap.val();
+        console.log(role);
+    },
+
+    handleJoin() {
+        console.log('request to join');
+    },
+
+    handleCancel() {
+        console.log('cancel request');
+    },
+
+    isOwnProject() {
+        const { project, user } = this.props;
+        return project.ownerId === user.uid;
     },
 
     renderButton() {
-        if (this.props.project.role) {
+        if (this.state.loading) {
+            return null;
+        }
+
+        const classNames = purebem.many(block('button'), 'button-primary');
+
+        return (
+            <button type="button" className={ classNames } onClick={ this.handleClick }>Join</button>
+        );
+    },
+
+    renderSpinner() {
+        if (!this.state.loading) {
             return null;
         }
         return (
-            <button type="button" className={ block('button') } onClick={ this.handleClick }>Join</button>
+            <Spinner type="circle" />
         );
     },
 
     render() {
-        // if (this.isProjectOwner()) {
-        //     return null;
-        // }
-
         return (
-            <div className={ block() }>[button]</div>
+            <div className={ block() }>
+                { this.renderSpinner() }
+                { this.renderButton() }
+            </div>
         );
     }
 
@@ -105,10 +88,4 @@ const mapStateToProps = (state) => {
     };
 };
 
-const mapDispatchToProps = (dispatch, props) => {
-    return {
-        onUpdate: (role) => dispatch(resultUpdate(props.index, role))
-    };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(JoinButton);
+export default connect(mapStateToProps)(JoinButton);
