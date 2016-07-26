76886e286603354aa652441bbb4d5f22112336b9
Mikael Carlsson <mickec79@gmail.com>
2016-07-26 15:29:05 +0200

    prep work for new view

--- app/views/ProjectRingsView.js
+++ app/views/ProjectRingsView.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import purebem from 'purebem';
 
+import { Button, FormGroup, InputField, ViewHeader } from 'app/components';
+
+
+const block = purebem.of('project-rings-view');
 
 const ProjectRingsView = React.createClass({
+
+    propTypes: {
+
+    },
+
+    onInput() {
+
+    },
+
+    onSave() {
+
+    },
+
     render() {
         return (
-            <h1>Rings</h1>
+            <div className={ block() }>
+                <ViewHeader title="Rings &amp; Serial Numbers" />
+                <form className={ block('form') }>
+                    <FormGroup label="Ring Size">
+                        <InputField onChange={ this.onInput } />
+                    </FormGroup>
+                    <FormGroup label="Serial Number">
+                        <InputField onChange={ this.onInput } />
+                    </FormGroup>
+                    <FormGroup label="&nbsp;">
+                        <Button style="success">Save</Button>
+                    </FormGroup>
+                </form>
+            </div>
         );
     }
 });
