6fd0762b9c21ead63e4cbb7ad2c80a90bd4e2dcd
Mikael Carlsson <mickec79@gmail.com>
2016-08-06 01:54:11 +0200

    entry view WIP

--- app/views/ProjectEntryView.js
+++ app/views/ProjectEntryView.js
@@ -1,12 +1,97 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import purebem from 'purebem';
 
+import { ButtonToggle, FormGroup, InputField, ViewHeader } from 'app/components';
+
+
+const block = purebem.of('project-entry-view');
 
 const ProjectEntryView = React.createClass({
+
+    propTypes: {
+
+    },
+
+    handleInput(evt) {
+        console.log('input', evt.target.value);
+    },
+
+    handleToggle() {
+        console.log('toggle');
+    },
+
+    renderForm() {
+        return (
+            <div className={ block('form') }>
+                <ButtonToggle
+                    active="New Ring"
+                    center={ true }
+                    onClick={ this.handleToggle }
+                    options={ ['New Ring', 'Old Ring'] } />
+                <FormGroup label="Ring Size / ID">
+                    <InputField onChange={ this.handleInput } />
+                </FormGroup>
+                <FormGroup label="Species">
+                    <InputField onChange={ this.handleInput } />
+                </FormGroup>
+                <FormGroup label="Net">
+                    <InputField onChange={ this.handleInput } />
+                </FormGroup>
+                <FormGroup label="Age">
+                    <ButtonToggle
+                        active="1.0"
+                        onClick={ this.handleToggle }
+                        options={ ['1.0', '2.0', '2+', '3+'] } />
+                </FormGroup>
+                <FormGroup label="Sex">
+                    <ButtonToggle
+                        active="F"
+                        onClick={ this.handleToggle }
+                        options={ ['F', 'M'] } />
+                </FormGroup>
+                <FormGroup label="PJM">
+                    <InputField onChange={ this.handleInput } />
+                </FormGroup>
+                <FormGroup label="Fat">
+                    <InputField onChange={ this.handleInput } />
+                </FormGroup>
+                <FormGroup label="Sign.">
+                    <InputField onChange={ this.handleInput } />
+                </FormGroup>
+                <FormGroup label="Weight">
+                    <InputField onChange={ this.handleInput } />
+                </FormGroup>
+                <FormGroup label="Wing length">
+                    <InputField onChange={ this.handleInput } />
+                </FormGroup>
+                <FormGroup label="Hand">
+                    <InputField onChange={ this.handleInput } />
+                </FormGroup>
+                <FormGroup label="Arm">
+                    <InputField onChange={ this.handleInput } />
+                </FormGroup>
+            </div>
+        );
+    },
+
     render() {
         return (
-            <h1>Entry</h1>
+            <div className={ block() }>
+                <ViewHeader title="New Entry" />
+                { this.renderForm() }
+            </div>
         );
     }
+
 });
 
-export default ProjectEntryView;
+const mapStateToProps = (state) => {
+    return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProjectEntryView);
