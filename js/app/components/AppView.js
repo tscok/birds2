92d54d5a62d5922ed97320f1526225cfbe9d8ede
Mikael Carlsson <mickec79@gmail.com>
2016-09-29 09:25:14 +0200

    basic styling of appView and rendering of nav

--- js/app/components/AppView.js
+++ js/app/components/AppView.js
@@ -1,6 +1,8 @@
 import React, { PropTypes } from 'react';
 import purebem from 'purebem';
 
+import { isNullOrEmpty } from 'js/utils';
+
 import auth from 'js/redux/user/auth';
 import { NavigationView } from 'js/navigation/components';
 
@@ -10,14 +12,27 @@ const block = purebem.of('app-view');
 const AppView = React.createClass({
 
     propTypes: {
+        auth: PropTypes.object,
         children: PropTypes.node
+    },
+
+    isUser() {
+        return !isNullOrEmpty(this.props.auth.uid);
+    },
+
+    renderNavigation() {
+        if (!this.isUser()) {
+            return null;
+        }
+
+        return (<NavigationView />);
     },
 
     render() {
         return (
             <div className={ block() }>
                 <header className={ block('header') }>
-                    <NavigationView />
+                    { this.renderNavigation() }
                 </header>
                 <main className={ block('main') }>
                     { this.props.children }
