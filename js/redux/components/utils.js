37470ffe255d12d3ed7da0aa968bdfd4c26e5e6f
Mikael Carlsson <mickec79@gmail.com>
2017-03-13 22:17:01 +0100

    Utility function for getting component state.

--- js/redux/components/utils.js
+++ js/redux/components/utils.js
@@ -1,3 +1,10 @@
+import get from 'lodash.get';
+
+
 export function getPath({ root, path } = {}) {
     return root.split('.').concat(path.split('.'));
+};
+
+export function getComponent(state, props) {
+    return get(state, getPath({ ...props }));
 };
